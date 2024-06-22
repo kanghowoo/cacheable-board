6290834320e81bb6c31e6298bc409a8fe66992de
kanghowoo <kanghowoo@github.com>
2024-06-22 16:36:55 +0900

    init project

---
+++ src/test/java/com/mide/gangsaeng/GangsaengApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mide.gangsaeng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GangsaengApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
