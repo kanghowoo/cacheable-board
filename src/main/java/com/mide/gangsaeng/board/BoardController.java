f83420abf8f524be66f464a6db0680923331b55a
kanghowoo <kanghowoo@github.com>
2024-06-22 18:35:35 +0900

    feat: board write api

---
+++ src/main/java/com/mide/gangsaeng/board/BoardController.java
@@ -0,0 +1,24 @@
+package com.mide.gangsaeng.board;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/board")
+public class BoardController {
+
+    private final BoardService boardService;
+
+    @Autowired
+    public BoardController(BoardService boardService) {this.boardService = boardService;}
+
+    @PostMapping
+    public ResponseEntity<String> upload() {
+        boardService.write();
+        return ResponseEntity.ok("ok");
+    }
+}
