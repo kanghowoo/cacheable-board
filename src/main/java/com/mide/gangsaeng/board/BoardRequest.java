87d5d3f7766810b64ecdc7c6443938b083dbae29
kanghowoo <kanghowoo@github.com>
2024-06-25 02:25:05 +0900

    feat: add validation for nullable, empty requests.

--- src/main/java/com/mide/gangsaeng/board/BoardRequest.java
+++ src/main/java/com/mide/gangsaeng/board/BoardRequest.java
@@ -1,12 +1,17 @@
 package com.mide.gangsaeng.board;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class BoardRequest {
+    @NotBlank(message = "제목을 입력해 주세요.")
     private String title;
+    @NotEmpty(message = "내용을 입력해 주세요.")
     private String content;
+    @NotBlank
     private int userId;
 }
