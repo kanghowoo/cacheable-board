114bee584c3b92062099b0412f04bcbc6a290741
kanghowoo <kanghowoo@github.com>
2024-06-30 18:51:52 +0900

    style: unused import delete

--- src/main/java/com/mide/gangsaeng/board/BoardRepositoryImpl.java
+++ src/main/java/com/mide/gangsaeng/board/BoardRepositoryImpl.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
-import com.mide.gangsaeng.common.Cursor;
 
 @Repository
 public class BoardRepositoryImpl implements BoardRepository {
