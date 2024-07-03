bd7a7eda9925804a524d738115b365bb1ffe7e9e
kanghowoo <kanghowoo@github.com>
2024-07-03 23:59:42 +0900

    fix: concurrenthashmap move to class vairable and business error message added.

--- src/main/java/com/mide/gangsaeng/bannedword/BannedWordServiceImpl.java
+++ src/main/java/com/mide/gangsaeng/bannedword/BannedWordServiceImpl.java
@@ -8,28 +8,28 @@ import org.springframework.stereotype.Service;
 @Service
 public class BannedWordServiceImpl implements BannedWordService {
 
-    private static ConcurrentHashMap<String,BannedWord> cache;
-
-    public BannedWordServiceImpl() {
-        cache = new ConcurrentHashMap<>();
-    }
+    private static final ConcurrentHashMap<String,BannedWord> cache = new ConcurrentHashMap<>();
 
     @Override
-    public void add(String word) {
+    public String add(String word) {
         if (isBanned(word)) {
-            throw new IllegalArgumentException("Already added.");
+            return "Already added.";
         }
 
         cache.put(word, new BannedWord(word));
+
+        return "success";
     }
 
     @Override
-    public void delete(String word) {
+    public String delete(String word) {
         if (!isBanned(word)) {
-            throw new IllegalArgumentException("Not added.");
+            return "Not added.";
         }
 
         cache.remove(word);
+
+        return "success";
     }
 
     @Override
