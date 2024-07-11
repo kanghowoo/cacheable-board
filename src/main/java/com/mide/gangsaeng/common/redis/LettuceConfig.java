36baf0f922cd401c9e01e33b389f8995af69bb35
kanghowoo <kanghowoo@github.com>
2024-07-11 21:40:23 +0900

    fix: redis client set option when disconnected behavior REJECT_COMMANDS

--- src/main/java/com/mide/gangsaeng/common/redis/LettuceConfig.java
+++ src/main/java/com/mide/gangsaeng/common/redis/LettuceConfig.java
@@ -5,12 +5,16 @@ import java.time.Duration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.ClientOptions.DisconnectedBehavior;
 import io.lettuce.core.RedisClient;
 import io.lettuce.core.RedisURI;
 import io.lettuce.core.api.StatefulRedisConnection;
 import io.lettuce.core.api.sync.RedisCommands;
 import jakarta.annotation.PreDestroy;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Configuration
 public class LettuceConfig {
     private RedisClient redisClient;
@@ -19,6 +23,11 @@ public class LettuceConfig {
     @Bean
     public RedisCommands<String, String> syncCommands() {
         this.redisClient = RedisClient.create(getRedisURI());
+        redisClient.setOptions(ClientOptions.builder()
+                                            .disconnectedBehavior(
+                                                    DisconnectedBehavior.REJECT_COMMANDS)
+                                            .build());
+
         this.connection = redisClient.connect();
         return this.connection.sync();
     }
@@ -35,6 +44,6 @@ public class LettuceConfig {
     }
 
     private RedisURI getRedisURI() {
-        return new RedisURI("localhost", 6379, Duration.ofSeconds(20));
+        return new RedisURI("localhost", 6379, Duration.ofSeconds(60));
     }
 }
